6196c93663942bfaebf863d8d46e4548ec976fbc
rifat-54 <rifat999por@gmail.com>
2026-01-07 15:06:10 +0600

    impliment better auth

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import express from "express"
 import { postRouter } from "./modules/post/post.router"
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth";
-import cookieParser from "cookie-parser";
+// import cookieParser from "cookie-parser";
 
 const app=express()
 app.use(express.json())
-app.use(cookieParser()); 
-app.all('/api/auth/*splet', toNodeHandler(auth));
+// app.use(cookieParser()); 
+
+app.all('/api/auth/*splat', toNodeHandler(auth));
+
 // app.all(/^\/api\/auth\/.*$/, toNodeHandler(auth));
 
 app.use("/posts",postRouter)
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,17 +2,13 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
 
-console.log('sugnup');
+
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql", // or "mysql", "postgresql", ...etc
     }),
     emailAndPassword: { 
     enabled: true, 
-  },
-  trustedOrigins: [
-    "http://localhost:5173",
-    "http://localhost:5000"
-  ],
+  }
   
 });
