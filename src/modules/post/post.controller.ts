cf8cef6676791e8c8c2eca75d53adc7aa581555e
rifat-54 <rifat999por@gmail.com>
2026-01-14 22:57:40 +0600

    impliment search filtering

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { postServices } from "./post.services";
-
+import { PostStatus } from "../../../generated/prisma/enums";
 
 const createPost = async (req: Request, res: Response) => {
   try {
@@ -12,7 +12,6 @@ const createPost = async (req: Request, res: Response) => {
     }
     const result = await postServices.createPost(req.body, req.user.id);
     res.status(201).json(result);
-
   } catch (error) {
     res.status(400).json({
       error: "failed Post",
@@ -23,19 +22,36 @@ const createPost = async (req: Request, res: Response) => {
 
 const getAllPost = async (req: Request, res: Response) => {
   try {
-    const{search}=req.query
-    const searchString=typeof search ==='string'?search :undefined
-    const tags=req.query.tags?(req.query.tags as string).split(",") : []
+    const { search } = req.query;
+    const searchString = typeof search === "string" ? search : undefined;
+    const tags = req.query.tags ? (req.query.tags as string).split(",") : [];
+    const isFeatured = req.query.isFeatured
+      ? req.query.isFeatured === "true"
+        ? true
+        : req.query.isFeatured === "false"
+        ? false
+        : undefined
+      : undefined;
 
-    const result = await postServices.getAllPost({search:searchString,tags})
+      const status=req.query.status as PostStatus | undefined
+
+      const authorId=req.query.authorId as string | undefined
+
+    const result = await postServices.getAllPost({
+      search: searchString,
+      tags,
+      isFeatured,
+      status,
+      authorId
+    });
+
     res.status(200).json(result);
-
   } catch (error) {
     res.status(400).json({
-        success:false,
-        message:"server error",
-        details:error
-    })
+      success: false,
+      message: "server error",
+      details: error,
+    });
   }
 };
 
--- src/modules/post/post.services.ts
+++ src/modules/post/post.services.ts
@@ -1,4 +1,5 @@
-import { Post } from "../../../generated/prisma/client";
+import { Post, PostStatus } from "../../../generated/prisma/client";
+import { PostWhereInput } from "../../../generated/prisma/models";
 import { prisma } from "../../lib/prisma";
 
 const createPost = async (
@@ -14,32 +15,69 @@ const createPost = async (
   return result;
 };
 
-const getAllPost = async (payload:{search: string | undefined,tags:string[] | []}) => {
-  const data = await prisma.post.findMany({ 
+const getAllPost = async (payload: {
+  search: string | undefined
+  tags: string[] | []
+  isFeatured:boolean | undefined,
+  status:PostStatus | undefined,
+  authorId:string | undefined
+}) => {
+  const andCondition:PostWhereInput[]=[]
+  if(payload.search){
+    andCondition.push(
+      {
+          OR: [
+            {
+              title: {
+                contains: payload.search as string,
+                mode: "insensitive",
+              },
+            },
+            {
+              content: {
+                contains: payload.search as string,
+                mode: "insensitive",
+              },
+            },
+            {
+              tags: {
+                has: payload.search as string,
+              },
+            },
+          ],
+        },
+    )
+  }
+  if(payload.tags){
+    andCondition.push(
+      {
+          tags: {
+            hasEvery: payload.tags,
+          },
+        },
+    )
+  }
+
+  if(typeof payload.isFeatured ==='boolean'){
+    andCondition.push({
+      isFeatured:payload.isFeatured
+    })
+  }
+
+  if(payload.status){
+    andCondition.push({
+      status:payload.status
+    })
+  }
+
+if(payload.authorId){
+  andCondition.push({
+    authorId:payload.authorId
+  })
+}
+  const data = await prisma.post.findMany({
     where: {
-      OR: [
-        {
-          title: {
-            contains: payload.search as string,
-            mode: "insensitive",
-          }
-        },
-        {
-          content: {
-            contains: payload.search as string,
-            mode: "insensitive",
-          }
-        },
-        {
-            tags:{
-                has:payload.search as string
-                
-            }
-        }
-      ],
-      tags:{
-        hasEvery:tags
-      }
+      AND: andCondition
     },
   });
   return data;
